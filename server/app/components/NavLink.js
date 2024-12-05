9347e6170643e76975c5e9e8356f7f71e1aed28f
vars-vars <vars6978@gmail.com>
2024-12-05 14:43:12 +0000

    navbar, trashcan 생성

--- server/app/components/NavLink.js
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function NavLink({ href, children }) {
-    return (
-        <Link href={href}>
-            <div style={{ textDecoration: 'none', color: 'blue' }}>{children}</div>
-        </Link>
-    );
-}
