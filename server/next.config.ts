d6e3524ee68e34193843d448fbb43598059c462f
vars-vars <vars6978@gmail.com>
2024-12-14 13:12:14 +0000

    불필요 파일 제거

--- server/next.config.ts
+++ server/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+		ignoreDuringBuilds: true, // 빌드 중 ESLint 경고 무시
+	},
 };
 
 export default nextConfig;
